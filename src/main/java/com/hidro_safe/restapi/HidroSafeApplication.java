917b46c9feed208a17ff8a7e191588b695fc7a2f
Gigomeni0 <joaoantonio.goncalves2003@gmail.com>
2025-05-27 00:49:08 -0300

    Renaming RestapiApplication to HidroSafeApplication for better alignment with project naming conventions

--- src/main/java/com/hidro_safe/restapi/HidroSafeApplication.java
+++ src/main/java/com/hidro_safe/restapi/HidroSafeApplication.java
@@ -1,13 +1,12 @@
+// src/main/java/com/hidrosafe/restapi/HidroSafeApplication.java
 package com.hidro_safe.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestapiApplication {
-
+public class HidroSafeApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(RestapiApplication.class, args);
+		SpringApplication.run(HidroSafeApplication.class, args);
 	}
-
 }
